2abe3a448aab3246effd10d126a8d3fbee1625a8
1DmntdMnky <preetjha700@gmail.com>
2022-01-29 00:53:42 +0530

    Increased the duration of Framer in hackncode

--- src/Pages/HacknCode/hack-n-code.jsx
+++ src/Pages/HacknCode/hack-n-code.jsx
@@ -13,7 +13,7 @@ const Hack_n_code = () => {
     <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
-      transition={{ duration: 1, ease: "easeInOut" }}
+      transition={{ duration: 2, ease: "easeInOut" }}
     >
       <Hnc_header />
       <Hnc_prize />
