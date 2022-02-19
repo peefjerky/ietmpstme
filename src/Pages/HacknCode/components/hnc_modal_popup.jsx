52a9e35cd0f42e9ae29b05216af2cb018b199a82
1DmntdMnky <preetjha700@gmail.com>
2022-02-19 12:32:23 +0530

    Modal Popup HnC

---
+++ src/Pages/HacknCode/components/hnc_modal_popup.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import "../../../css/hnc_modal_popup.scss";
+import { Modal } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+import LaptopAnimation from "../../../components/laptopanimation";
+import HnCTypedReactHooks from "../../../components/hnc_animation";
+
+const Hnc_modal_popup = (props) => {
+  return (
+    <Modal
+      id="Hnc_Modal"
+      className="text-lg-start text-sm-center text-center text-md-center"
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton className="">
+        <Modal.Title id="contained-modal-title-vcenter">
+          Register For Hack-n-Code 5.0
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body className="HncModalBody p-5">
+        <div className="row mx-auto align-items-center">
+          <div className="col-lg-6 col-md-6 col-12">
+            <div>
+              <span className="badge my-2">24 Hour Hackathon</span>
+              <br />
+              <span className="badge my-2">26th - 27th March</span>
+            </div>
+            <div className="hnc_typist mt-5">
+              <HnCTypedReactHooks />
+            </div>
+            <div>
+              <p className="mb-5">
+                We share knowledge in the fields of engineering and technology
+                to encourage students to find new and innovative solutions to
+                problems which have plagued mankind for decades.
+              </p>
+              <button
+                className="mx-3 d-inline-block align-items-center"
+                id="btn"
+              >
+                <a
+                  className="text-light stretched-link"
+                  href="https://lu.ma/HacknCode"
+                >
+                  Register Now!
+                </a>
+              </button>
+            </div>
+          </div>
+
+          <div
+            className="col-sm-12 col-md-8 col-lg-6 mt-md-5 mt-sm-5 align-itmes-center mx-auto"
+            id="hnc_svgimg"
+          >
+            <LaptopAnimation />
+          </div>
+        </div>
+      </Modal.Body>
+      {/*  <Modal.Footer >
+        <Button
+          onClick={props.onHide}>Register Now</Button>
+      </Modal.Footer> */}
+    </Modal>
+  );
+};
+export default Hnc_modal_popup;
