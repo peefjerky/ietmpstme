8de81904c5fd7665f7ae47b3567a63dd1a1717ee
zaidmukaddam <zaidaltafmukaddam@gmail.com>
2022-02-28 22:39:52 +0530

    feat: add sponsers

--- src/Pages/HacknCode/components/hnc_sponsors.jsx
+++ src/Pages/HacknCode/components/hnc_sponsors.jsx
@@ -1,5 +1,8 @@
 import React /* , { useEffect } */ from "react";
 import "../../../css/hnc_sponsors.scss";
+import github from "../../../Assets/Images/GitHub.png";
+import earthhack from "../../../Assets/Images/Earth+Hacks.webp";
+import mlsa from "../../../Assets/Images/mlsa.webp";
 /* import AOS from "aos";
 import "aos/dist/aos.css"; */
 const Hnc_sponsors = () => {
@@ -14,8 +17,27 @@ const Hnc_sponsors = () => {
         className="row mx-5 align-items-center text-center"
         id="sponsorsContanier"
       >
-        <div className="col-lg-12 col-sm-12 col-md-12">
-          <h3 className="text-white align-middle my-5">Coming Soon!</h3>
+        <div className="d-flex flex-row sponn">
+          <img
+            src={github}
+            alt="sponsor1"
+            className="img-fluid"
+            style={{ height: "100px", width: "100px", marginRight: "5rem", marginLeft: "5rem" }}
+          />
+
+          <img
+            src={earthhack}
+            alt="sponsor2"
+            className="img-fluid"
+            style={{ height: "100px", width: "450px", marginRight: "5rem", marginLeft: "8rem" }}
+          />
+
+          <img
+            src={mlsa}
+            alt="sponsor3"
+            className="img-fluid"
+            style={{ height: "150px", marginRight: "5rem", marginLeft: "5rem" }}
+          />
         </div>
       </div>
     </div>
